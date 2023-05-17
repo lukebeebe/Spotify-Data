0f5c2aad5da3b45ee5d7288a40476588a8c18a7b
Luke Beebe <97060937+lukebeebe@users.noreply.github.com>
2023-05-16 23:58:19 -0400

    Update Guessing Game.py

--- Guessing Game.py
+++ Guessing Game.py
@@ -27,15 +27,24 @@ def getLocation(ip):
     }
     return location_data
 def findSong(year, month, day):
+    highestTime=0
     if year == 0:
-        print("This may take awhile...")
-        
+        print(str(len(spotifyDf['master_metadata_album_artist_name'].unique()))+" unique artists")
+        for artist in spotifyDf['master_metadata_album_artist_name'].unique():
+            artistTime=sum(spotifyDf["ms_played"][spotifyDf["master_metadata_album_artist_name"]==artist])
+            print(str(artistTime)+" "+artist)
+        if artistTime>highestTime:
+            mostPopular=artist
+            highestTime=artistTime
+            print(mostPopular+" "+str(artistTime))
+        hours=round(highestTime/1000/60/60,1)
+        print(mostPopular+" "+str(hours)+" hours")
 def guessingGame():
     print("Welcome to the Guessing Game! ©")
     time.sleep(1)
     print("We will provide you with a location ")
 def explore():
-    year, month, day = input("Provide a date in the form '1996:07:26'                   ").split(sep=":")
+    year, month, day = input("Provide a date in the form '1996:07:26'                         ").split(sep=":")
     year, month, day = int(year), int(month), int(day)
     findSong(year, month, day)
 
@@ -45,9 +54,10 @@ filepaths=["/Users/lukebeebe/Desktop/Amiri Data/endsong_0.json","/Users/lukebeeb
 
 #jsonDf
 spotifyDf=jsonDf(filepaths)
+spotifyDf=spotifyDf['ts',]
 spotifyDf['ts']=pd.to_datetime(spotifyDf['ts'])
 game=""
-while ("exp" not in game) and ("gues" not in game):
+while ("ex" not in game) and ("g" not in game):
     game=input("Would you like to explore your data or play the Guessing Game?  ").lower()
     time.sleep(1)
     if ("ex" not in game) and ("g" not in game):
