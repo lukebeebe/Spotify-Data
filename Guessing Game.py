d56ee5956ecdb17ff69c4185b2a6931a6e8be762
Luke Beebe <97060937+lukebeebe@users.noreply.github.com>
2023-05-17 17:36:33 -0400

    Update Guessing Game.py

--- Guessing Game.py
+++ Guessing Game.py
@@ -3,7 +3,6 @@ import json
 import pandas as pd
 import datetime
 import calendar
-import random
 import time
 import requests
 
@@ -38,41 +37,61 @@ def findSong(spotifyDf):
     timesArtists=sorted(zip(times,artists),reverse=True)
     return timesArtists
 
-
-def explore(spotifyDf):
-    year, month, day = 0, 0, 0
-    while (year not in spotifyDf.ts.dt.year.unique()) or (month not in spotifyDf.ts.dt.month.unique() or (day not in spotifyDf.ts.dt.day.unique())):
-        year, month, day = input("Provide a date in the form '1996:07:26'                         ").split(sep=":")
-        print(year+" "+month+" "+day)
-        year, month, day = int(year), int(month), int(day)
-        try:
-            spotifyDf=spotifyDf[(spotifyDf.ts.dt.year==year) and (spotifyDf.ts.dt.month==month) and (spotifyDf.ts.dt.day==day)]
-        except:
-            print("Invalid day or month or year")
-            try:
-                spotifyDf=spotifyDf[(spotifyDf.ts.dt.year==year) and (spotifyDf.ts.dt.month==month)]
-            except:
-                print("Invalid month or year")
-                try:
-                    spotifyDf=spotifyDf[(spotifyDf.ts.dt.year==year)]
-                except:
-                    print("Invalid year")
-    popular=findSong(spotifyDf)
-    print(popular[0:5])
-
-
-
-
-filepaths=["/Users/lukebeebe/Desktop/Amiri Data/endsong_0.json","/Users/lukebeebe/Desktop/Amiri Data/endsong_1.json","/Users/lukebeebe/Desktop/Amiri Data/endsong_2.json","/Users/lukebeebe/Desktop/Amiri Data/endsong_3.json","/Users/lukebeebe/Desktop/Amiri Data/endsong_4.json","/Users/lukebeebe/Desktop/Amiri Data/endsong_5.json","/Users/lukebeebe/Desktop/Amiri Data/endsong_6.json","/Users/lukebeebe/Desktop/Amiri Data/endsong_7.json"]
-
-#jsonDf
-spotifyDf=jsonDf(filepaths)
-spotifyDf=spotifyDf[["ts","master_metadata_album_artist_name","ms_played","ip_addr_decrypted"]].dropna()
-spotifyDf['ts']=pd.to_datetime(spotifyDf['ts'])
-
-print("Welcome to the Guessing Game! ©")
-time.sleep(1)
-print("We will provide you with a location and day.")
-time.sleep(1)
-print("You have three tries to guess an artist you listened to.")
-time.sleep(1)
+keepPlaying='y'
+while keepPlaying=="y":
+    filepaths=["/Users/lukebeebe/Desktop/Amiri Data/endsong_0.json","/Users/lukebeebe/Desktop/Amiri Data/endsong_1.json","/Users/lukebeebe/Desktop/Amiri Data/endsong_2.json","/Users/lukebeebe/Desktop/Amiri Data/endsong_3.json","/Users/lukebeebe/Desktop/Amiri Data/endsong_4.json","/Users/lukebeebe/Desktop/Amiri Data/endsong_5.json","/Users/lukebeebe/Desktop/Amiri Data/endsong_6.json","/Users/lukebeebe/Desktop/Amiri Data/endsong_7.json"]
+    remember=""
+    #jsonDf
+    spotifyDf=jsonDf(filepaths)
+    spotifyDf=spotifyDf[["ts","master_metadata_track_name","master_metadata_album_artist_name","ms_played","ip_addr_decrypted"]].dropna()
+    spotifyDf['ts']=pd.to_datetime(spotifyDf['ts'])
+    spotifyDf=spotifyDf[spotifyDf["master_metadata_album_artist_name"]!=remember]
+    print("Welcome to the Guessing Game ©")
+    time.sleep(1)
+    print("We provide you with a date")
+    time.sleep(1)
+    print("You name an artist you listened to on that day")
+    time.sleep(1)
+    print("You have three attempts. Good luck!")
+    time.sleep(2)
+    song=spotifyDf.sample()
+    year=song.ts.dt.year.tolist()[0]
+    month=song.ts.dt.month.tolist()[0]
+    monthName=calendar.month_name[month]
+    day=song.ts.dt.day.tolist()[0]
+    date=monthName+" "+str(day)+", "+str(year)
+    print(date)
+    spotifyDf=spotifyDf[(spotifyDf.ts.dt.year==year) & (spotifyDf.ts.dt.month==month) & (spotifyDf.ts.dt.day==day)]
+    artists=spotifyDf['master_metadata_album_artist_name'].unique()
+    artistString=""
+    for artist in artists:
+        artistString+=artist
+        artistString+=" "
+    artistString=artistString.lower().replace(",","")
+    for i in range(0,3):
+        if i==2:
+            song=spotifyDf.sample()
+            hint=song['master_metadata_track_name'].values[0]
+            print("Hint: "+hint)
+            time.sleep(2)
+        guess=input("").lower().replace(",","")
+        if (guess in artistString) & (len(guess.lower()) > 3):
+            print("Correct!")
+            correct=True
+            time.sleep(1)
+            break
+        else:
+            print("Incorrect "+str(i+1))
+            correct=False
+            time.sleep(1)
+    if (i>=2) and (correct==False):
+        print(hint+" was by "+song['master_metadata_album_artist_name'].values[0])
+        time.sleep(2)
+    print("You could have named:")
+    for artist in artists:
+        print(" "+artist)
+        time.sleep(0.2)
+        if guess in artist.lower().replace(",",""):
+            remember=artist
+    print("")
+    time.sleep(2)
